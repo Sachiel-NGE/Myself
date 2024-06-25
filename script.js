ee99b56d0ec07630693e9380938fcfbf9dd43267
Sachiel-NGE <oriharaizaya242@gmail.com>
2024-06-25 16:08:45 +0100

    link

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const Button = document.getElementById('mybutton');
-const myVideo = document.getElementById('Video');
-
-Button.addEventListener('click', () => {
-    myVideo.style.display = 'block'; // Show the video
-    myVideo.play();
-});
-
-myVideo.addEventListener('ended', () => {
-    // Replace 'next-page.html' with your actual next page URL
-    window.location.href = 'https://sachiel-nge.github.io/Myself/BetterAnimeList/Layout.html';
-});
+document.getElementById('mybutton').onclick = function () {
+    document.getElementById('video').play();
+};
